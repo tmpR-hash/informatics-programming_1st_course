259698c188725b6e1662f971d7a85d936fa7ff20
tmpR-hash <efimovart.2006@gmail.com>
2025-05-26 12:30:20 +0400

    feat: extended message

--- test.cpp
+++ test.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "wussap maboy" << endl << endl;
+    cout << "wussap maboy\n here u can find my solutions (Zaboev approved) for c++ tasks of I&P course" << endl << endl;
     return 0;
 }
